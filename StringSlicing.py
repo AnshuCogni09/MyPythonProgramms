077cde0698a1e96344b8df6f04fd14501138bf5b
Panigrahi <2464396@cognizant.com>
2026-01-10 19:18:29 +0530

    Added StringSlicing

---
+++ StringSlicing.py
@@ -0,0 +1,7 @@
+name = "Anshumaan Panigrahi"
+print(len(name))  # Length of the string
+print(name[0:10]) # Slicing from index 0 to 9
+print(name[10::2])  # Slicing from index 9 to the end
+print(name[::3])  # Slicing with a step of 3
+print(name[::-1])  # Reversing the string
+print(name[-10:len(name)])  # Slicing using negative indices
